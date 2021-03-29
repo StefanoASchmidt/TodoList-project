c8c2864e0c5d5aa07941cb2c136156e3956b67fd
Stefano Schmidt <schmidt.stefano@outlook.com>
2021-03-29 13:01:25 +0200

    Updated todolist.js

--- todolist.js
+++ todolist.js
@@ -90,12 +90,75 @@ class TodoList {
     return this.todos.reduce((acc, todo) => `${acc}${todo}\n`, title);
   }
 
+  forEach(callback) {
+    this.todos.forEach(callback);
+  }
+
+  filter(callback) {
+    let filteredList = new TodoList(this.title);
+    this.todos.filter(callback).forEach(todo => filteredList.add(todo));
+    return filteredList;
+  }
+
+  findByTitle(title) {
+    return this.filter(todo => todo.getTitle() === title).first();
+  }
+
+  allDone() {
+    return this.filter(todo => todo.isDone());
+  }
+
+  allNotDone() {
+    return this.filter(todo => !todo.isDone());
+  }
+
+  markDone(title) {
+    let todo = this.findByTitle(title);
+    if (todo !== undefined) {
+      todo.markDone();
+    }
+  }
+
+  markAllDone() {
+    this.forEach(todo => todo.markDone());
+  }
+
+  markAllUndone() {
+    this.forEach(todo => todo.markUndone());
+  }
+
+  toArray() {
+    return this.todos.slice();
+  }
+
   _validateIndex(index) {
     if (!(Object.keys(this.todos).includes(String(index)))) {
       throw new ReferenceError(`invalid index: ${index}`);
     }
   }
 }
+
+let todo1 = new Todo("Buy milk");
+let todo2 = new Todo("Clean room");
+let todo3 = new Todo("Go to the gym");
+let todo4 = new Todo("Go shopping");
+let todo5 = new Todo("Feed the cats");
+let todo6 = new Todo("Study for Launch School");
+let list = new TodoList("Today's Todos");
+
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
+list.add(todo4)
+list.add(todo5)
+list.add(todo6)
+todo1.markDone();
+todo5.markDone();
+
+console.log(list.filter(todo => todo.isDone()).first());
+// => Todo { title: 'Buy milk', done: true }
+
+
 
 
 
